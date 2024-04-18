aaeda5a01b957460c188f66dcca61e0e4ce55d7e
Gaurav Kumar <138664658+gauravfrontendproject@users.noreply.github.com>
2024-04-18 17:38:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Resturant from './component/Basic/Resturant';
+const App = () => {
+  return <>
+    <Resturant />
+  </>
+}
+
+export default App
---
+++ src/component/Basic/menuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/maggi.jpg",
+        name: "maggi",
+        category: "breakfast",
+        price: "12₹",
+        description:
+            "Maggi's versatility allows restaurants to get creative with their offerings, from classic preparations like Maggi Masala and Maggi with vegetables to more inventive creations like Maggi pizza or Maggi stir-fry. The simplicity and speed of preparation make it an ideal choice for busy kitchens, ensuring prompt service without compromising on taste.",
+    },
+
+    {
+        id: 2,
+        image: "images/allupakoida.jpg",
+        name: "allu pakoida",
+        category: "evening",
+        price: "20₹",
+        description:
+            "Experience the crispy indulgence of our Allu Pakoida, a timeless favorite that promises satisfaction in every bite. From its golden exterior to its flavorful interior, each piece is a testament to our commitment to quality and taste. Discover culinary perfection at your fingertips in our restaurant app today!",
+    },
+    {
+        id: 3,
+        image: "images/corn.jpg",
+        name: "corn",
+        category: "breakfast",
+        price: "10₹",
+        description:
+
+            "Savor the sweetness of summer with our tantalizing corn delights. From classic buttered corn on the cob to innovative corn salads and savory corn fritters, each dish bursts with flavor and freshness. Indulge in the taste of sunshine in every bite, exclusively available in our restaurant app. Your corn cravings await!",
+    },
+    {
+        id: 4,
+        image: "../images/chola.jpg",
+        name: "chola",
+        category: "lunch",
+        price: "50₹",
+        description:
+            "Savor the rich flavors of our hearty Chole, a culinary masterpiece that captivates with every spoonful. Tender chickpeas simmered in a fragrant blend of spices, served piping hot alongside fluffy bhaturas or rice. Experience the essence of Indian comfort food in every bite, exclusively available in our restaurant app. Delight your palate today! ",
+    },
+    {
+        id: 5,
+        image: "../images/pizza.jpg",
+        name: "pizza",
+        category: "evening",
+        price: "80₹",
+        description:
+            "Experience the fusion of flavors with our innovative Chole Pizza, a delightful twist on a classic favorite. Savory chickpeas simmered in aromatic spices, generously layered atop a crisp pizza crust and topped with cheese and herbs. Indulge in the perfect harmony of Indian and Italian cuisine, exclusively available in our restaurant app. Taste the fusion today! ",
+    },
+    {
+        id: 6,
+        image: "../images/nonvegthali.jpg",
+        name: "Non-Veg Thali",
+        category: "dinner",
+        price: "180₹",
+        description:
+            "Delight in the abundance of flavors with our Non-Veg Thali, a symphony of culinary excellence on a single platter. Experience the rich diversity of meats, gravies, and accompaniments meticulously curated to tantalize your taste buds. Indulge in a feast for the senses, exclusively available in our restaurant app. Treat yourself to culinary bliss today ",
+    },
+    {
+        id: 7,
+        image: "../images/sweet.jpg",
+        name: "Sweets",
+        category: "dinner",
+        price: "60₹",
+        description:
+            "Indulge in the divine sweetness of our delectable sweets, crafted with care to tantalize your taste buds and satisfy your cravings.From traditional delicacies to innovative creations, each bite is a celebration of flavor and indulgence.Experience the joy of sweetness in every mouthful, exclusively available in our restaurant app.Treat yourself today!",
+    },
+    {
+        id: 8,
+        image: "../images/rajmarice.jpg",
+        name: "Rajma Rice",
+        category: "lunch",
+        price: "60₹",
+        description:
+            "Embark on a culinary journey with our soul-warming Rajma Rice, a comforting blend of tender kidney beans and aromatic spices served atop fluffy rice. Experience the rich flavors and hearty goodness that make every bite a moment of pure satisfaction. Discover the essence of home-cooked comfort in our restaurant app today! ",
+    },
+    {
+        id: 9,
+        image: "../images/samosa.jpg",
+        name: "samaso",
+        category: "evening",
+        price: "10₹",
+        description:
+            "Embrace the savory delight of our Samosas, crispy parcels of flavor that transport your taste buds to culinary paradise. Stuffed with aromatic spices and savory fillings, each bite is a symphony of texture and taste. Experience the perfect blend of tradition and innovation, exclusively available in our restaurant app. Dive into deliciousness today!",
+    },
+];
+
+export default Menu;
